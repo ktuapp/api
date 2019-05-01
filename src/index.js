c7129fd31a7491edd21b603f496277e760326296
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-05-02 00:18:17 +0530

    Minor fixes

--- src/index.js
+++ src/index.js
@@ -6,15 +6,13 @@ const PORT = process.env.PORT || 3000
 const startApp = () => {
   try {
     app.set('port', PORT)
-    app.listen(app.get('port'), () => console.log(`Server Running http://localhost:${PORT}`))
-		
-    
-    const expiry = new Date().setHours(48)
-    client.set('my string', 'this is a string', 'EX', expiry)
+    app.listen(app.get('port'), () => console.log(`Server Running http://localhost:${PORT}`))    
+    // const expiry = new Date().setHours(48)
+    // client.set('my string', 'this is a string', 'EX', expiry)
 
-    getAsync('my string').then((data) => {
-      console.log(data)
-    })
+    // getAsync('my string').then((data) => {
+    //   console.log(data)
+    // })
 		
   } catch (error) {
     console.error(`Error occured ${error}`)
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,4 +7,5 @@ const router = Router()
 router.get('/', (req, res) => res.json({ status: 'working' }))
 router.post('/api/v1/data', validateRequest, getStudentDetails)
 router.get('/api/v1/notifications', getNotifications)
+
 export default router
