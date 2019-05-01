6ff09242e7cb23d9b14f8fca4d34c47f97e0cffd
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-05-01 22:56:59 +0530

    Implementing node redis for saving cache

---
+++ src/core/redis.js
@@ -0,0 +1,22 @@
+import redis from 'redis'
+import { promisify } from 'util'
+require('dotenv').config()
+
+const client = redis.createClient({
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST
+})
+
+client.on('connect', () => {
+  // eslint-disable-next-line no-console
+  console.log('connected to redis')
+})
+
+client.on('error', function (err) {
+  // eslint-disable-next-line no-console
+  console.error('Error ' + err)
+})
+
+export const getAsync = promisify(client.get).bind(client)
+
+export default client
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
+/* eslint-disable no-console */
 import app from './core/app'
+import client, { getAsync } from './core/redis'
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
 const startApp = () => {
-	try {
-		app.set('port', PORT)
-		app.listen(app.get('port'), () => console.log(`Server Running http://localhost:${PORT}`))
-	} catch (error) {
-		console.error(`Error occured ${error}`)
-	}
+  try {
+    app.set('port', PORT)
+    app.listen(app.get('port'), () => console.log(`Server Running http://localhost:${PORT}`))
+		
+    
+    const expiry = new Date().setHours(48)
+    client.set('my string', 'this is a string', 'EX', expiry)
+
+    getAsync('my string').then((data) => {
+      console.log(data)
+    })
+		
+  } catch (error) {
+    console.error(`Error occured ${error}`)
+  }
 }
 
 startApp()
