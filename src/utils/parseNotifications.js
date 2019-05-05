2dbb215fa9ce597864cbe266d400ad14b9f9a320
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-05-05 20:15:15 +0530

    Fixes in parsing notification data

--- src/utils/parseNotifications.js
+++ src/utils/parseNotifications.js
@@ -21,9 +21,14 @@ export default ($) => {
             break
           case 1:
             if (i === 0) {
-              let value = $(elem).text()
+              const dataElem = $(elem).children().first()
+              notification.heading = $(dataElem).children().first().text()
+              let value = $(dataElem).clone().children().remove().end().text()
               value = value.replace(/\t/g, '')
               value = value.replace(/\n/g, '')
+              value = value.replace('Results', '')
+              value = value.replace('Notification', '')
+              value = value.replace(notification.heading, '')
               value = value.replace(/ {2}/g, '')
               notification.data = value
               notificationArray.push(notification)
