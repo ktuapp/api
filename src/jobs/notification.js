74caa321c453b1f61f7af16a629203d829d68e53
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-06-29 20:12:01 +0530

    More logs

--- src/jobs/notification.js
+++ src/jobs/notification.js
@@ -4,6 +4,8 @@ import parseNotifications from '../utils/parseNotifications.js'
 import { saveNotifications } from '../utils/redis'
 
 const fetchNotifications = async () => {
+  console.log("Fetching Notifications");
+  
   const options = {
     uri: 'https://ktu.edu.in/eu/core/announcements.htm',
     transform: function (body) {
@@ -13,6 +15,7 @@ const fetchNotifications = async () => {
   try {
     const response = await rp(options)
     const notifications = parseNotifications(response)
+    console.log("Fetching Notifications", notifications)
     saveNotifications(notifications)
   } catch (e) {
     console.error(e)
