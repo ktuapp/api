da534564e8c5ee020e78ce2a0810463d20e9a23e
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-06-29 20:10:51 +0530

    Adding a log in middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,6 +3,8 @@ const API_KEY = process.env.API_KEY || ''
 export const validateRequest = (req, res, next) => {
   if (req.body.key === API_KEY)
     next()
-  else
+  else {
+    console.log(req.body.key, API_KEY)
     res.status(403).send({ status: 'unauthorized' })
+  }
 }
