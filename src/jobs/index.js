dd5dbefc20d58ad1ecc7883df145f981ea81c68d
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-06-30 08:50:17 +0530

    Updated cron schedule time

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -2,7 +2,7 @@ import cron from 'node-cron'
 import notification from './notification'
 
 const startJobs = () => {
-  cron.schedule('* */3 * * *', notification)
+  cron.schedule('0 3,6,9,12 * * *', notification)
 }
 
 export default startJobs
